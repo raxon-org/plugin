bb058429cb1201b6705daa4efb84538ffbc67510
Remco van der Velde <remco@universeorange.com>
2026-01-29 15:48:31 +0100

    k

--- src/Route.php
+++ src/Route.php
@@ -816,7 +816,6 @@ trait Route {
         $options = (object) [];
         $flags = (object) [];
         foreach($data as $nr => $parameter){
-            d($parameter);
             $is_flag = false;
             $is_option = false;
             $is_array = false;
