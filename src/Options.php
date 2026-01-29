8858afae93bdc8d52f275b932a23fad3f3debef8
Remco van der Velde <remco@universeorange.com>
2026-01-29 15:34:35 +0100

    k

---
+++ src/Options.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author          Remco van der Velde
+ * @since           2026-01-28
+ * @copyright       (c) Remco van der Velde
+ * @license         MIT
+ * @version         1.0
+ */
+namespace Plugin;
+
+use Exception;
+use Exception\ObjectException;
+use Module\Core;
+use Module\Data;
+
+trait Options {
+
+    /**
+     * @throws Exception
+     */
+    public function options($attribute=null, $value=null): mixed
+    {
+        $prefix = __FUNCTION__;
+        $config = $this->config();
+        if($attribute!== null && $value !== null){
+            $config->set($prefix . '.' . $attribute, $value);
+        }
+        elseif($attribute !== null){
+            return $config->get($prefix . '.' . $attribute);
+        } elseif($config !== null) {
+            return $config->get($prefix);
+        }
+        return null;
+    }
+}
